71eaabf4dc9971901e998f6c5ff497c572459c14
Kishara Buddika <hathamunagekishara277@gmail.com>
2020-02-27 15:39:50 +0530

    Spring Boot with JPA Hibernate tool

---
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.model.Employee;
+import com.example.springboot.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
---
+++ src/main/java/com/example/springboot/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.example.springboot.service;
+
+import com.example.springboot.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+}
