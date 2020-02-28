51cd5de057ad7eb9444e94e295c5c625e80a6004
Kishara Buddika <hathamunagekishara277@gmail.com>
2020-02-28 15:22:03 +0530

    Auto Generated Tables and Add Employee using Postman Request http://127.0.0.1:8080/employee

--- src/main/java/com/example/springboot/controller/EmployeeController.java
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -3,9 +3,7 @@ package com.example.springboot.controller;
 import com.example.springboot.model.Employee;
 import com.example.springboot.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +22,10 @@ public class EmployeeController {
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
+
+    @PostMapping
+    public void addEmployee(@RequestBody Employee employee){
+        employeeService.addEmployee(employee);
+    }
+
 }
--- src/main/java/com/example/springboot/model/Employee.java
+++ src/main/java/com/example/springboot/model/Employee.java
@@ -1,11 +1,14 @@
 package com.example.springboot.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Employee {
     @Id
+    @GeneratedValue
     Integer id;
     String name;
     String department;
