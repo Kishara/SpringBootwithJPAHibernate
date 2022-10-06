e91a332ce02eede0958676e019a1f570726f8f31
Nyor Ja <botsot.felix@gmail.com>
2022-10-07 03:42:28 +0800

    Fix gradle wrapper and unit test

---
+++ src/test/java/com/example/springboot/controller/EmployeeControllerTest.java
@@ -0,0 +1,42 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.service.EmployeeService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureWebMvc
+@AutoConfigureMockMvc
+@WebMvcTest(EmployeeController.class)
+class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @DisplayName("Get all employees with empty result and status 200")
+    @Test
+    void testGetAllEmployees() throws Exception {
+
+        when(employeeService.getAllEmployees()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/employee"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+}
